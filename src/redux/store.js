dfc00288af8c2ea2c21d12256a23bf9b629c5fd9
iamkennis <kennisdamilare@gmail.com>
2020-11-27 00:14:15 +0100

    little update

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,11 @@ import logger from 'redux-logger'
 import { persistStore } from 'redux-persist'
 import rootReducer from './root-reducer'
 
-const middlerwares = [logger]
+const middlerwares = []
+
+if (process.env.NODE_ENV === 'development') {
+    middlerwares.push(logger)
+}
 
 export const store = createStore(rootReducer, applyMiddleware(...middlerwares))
 
